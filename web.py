554def2fa0c59abe98800fba715accd9fd254868
Redwan Diriye <ridwanmohamedco@outlook.com>
2021-10-15 09:31:02 -0500

    'Login/Register'

--- web.py
+++ web.py
@@ -1,7 +1,31 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 from flask import render_template
 from flask import request
+import smtplib
+
+from flask_wtf import form
+from forms import RegistrationForm, LoginForm
+
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///contacts.db'
+app.config['SECRET KEY'] = '693160797d3f60e229f048136274201fcba9337f7a988a1de3'
+
+# initialize database
+db = SQLAlchemy(app)
+
+# create db model
+class Contacts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+# Func to return string
+    def __repr__(self):
+        return '<Name %r' % self.id
+
 
 # posts = [
 #     {
@@ -27,6 +51,16 @@ def home():
 
 @app.route("/contacts", methods=["POST"])
 def contacts():
+
+# ADD EMAIL AND PASS IF YOU WANT THIS TO WORK
+    # msg = 'Welcome! we cover news out of this world'
+    # server = smtplib.SMTP("stmp.gmail.com", 587)
+    # server.starttls()
+    # server.login("virus.run.py@gmail.com")
+    # server.sendmail("virus.run.py@gmail.com", msg)
+
+
+
     first_name = request.form.get("first_name")
     last_name = request.form.get("last_name")
     phone_number = request.form.get("phone_number")
@@ -44,13 +78,19 @@ def contacts():
     last_name=last_name, 
     phone_number=phone_number)
 
+@app.route("/ring")
+def ring():
+   return render_template('ring.html')
+
 @app.route("/login")
 def login():
-    return render_template('login.html')
+    form = LoginForm
+    return render_template('login.html', title='Login', form=form)
 
 @app.route("/register")
 def register():
-    return render_template('register.html')
+    form = RegistrationForm
+    return render_template('register.html', title="Register", form=form)
 
 
 
