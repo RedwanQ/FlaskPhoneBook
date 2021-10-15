13c5fa915eb2e5376c4e4057a1a5a000244512aa
Redwan Diriye <ridwanmohamedco@outlook.com>
2021-10-14 20:27:16 -0500

    Done

--- web.py
+++ web.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 app = Flask(__name__)
 
 # posts = [
@@ -17,13 +18,27 @@ app = Flask(__name__)
 #     }
 # ]
 
+members = []
+
 @app.route("/")
 def home():
     return render_template('home.html')
 
-@app.route("/P#")
+@app.route("/P")
 def P():
     return render_template('app.html')
+
+@app.route("/contacts", methods=["POST"])
+def contacts():
+    first_name = request.form.get("first_name")
+    last_name = request.form.get("last_name")
+    phone_number = request.form.get("phone_number")
+    members.append(f" {first_name} {last_name} {phone_number} ")
+    title = 'Thank you!'
+    return render_template('info.html', title=title, first_name=first_name, last_name=last_name, phone_number=phone_number)
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
